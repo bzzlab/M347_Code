511c706974123ab75d26b5e91a1cc160d204fa59
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-05-16 14:36:29 +0200

    add 01-Solution

---
+++ 01/03_Solutions/02-sol.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Build
+docker build -t m347_i2 -f 02_dockerfile .
+# List images
+docker image ls
+# run
+docker run -itd --rm --name m347_c2 m347_i2
+# exec
+docker exec -it m347_c2 bash
+# stop container
+docker stop m347_c2
+# Remove image
+docker rmi m347_i2:latest
+# check removal
+docker image ls |grep "m347_i2"
