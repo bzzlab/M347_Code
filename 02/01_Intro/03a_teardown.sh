cbd5d40829824b2c945e55a6849362466b34cb9d
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-05-26 17:08:12 +0200

    02-Intro, Exer

--- 02/01_Intro/03a_teardown.sh
+++ 02/01_Intro/03a_teardown.sh
@@ -1,9 +1,16 @@
 #!/usr/bin/env bash
 #
-### Steps AFTER 03b_teardown.sh
-image=base_img3a
+### Steps to 01_dockerfile
+image=img3b
+container=cont3b
+# stop container
+docker stop "$container"
+sleep 3
+# Remove container
+id=$(docker ps -a |tail -n +2|xargs|cut -d' ' -f1)
+docker rm "$id"
 # Remove image
 docker rmi "$image":latest
 # check removal
-docker image ls
+docker image ls |grep "$image"
 
--- 02/01_Intro/03b_teardown.sh
+++ 02/01_Intro/03b_teardown.sh
@@ -1,16 +1,9 @@
 #!/usr/bin/env bash
 #
-### Steps to 01_dockerfile
-image=img3b
-container=cont3b
-# stop container
-docker stop "$container"
-sleep 3
-# Remove container
-id=$(docker ps -a |tail -n +2|xargs|cut -d' ' -f1)
-docker rm "$id"
+### Steps AFTER 03a_teardown.sh
+image=base_img3a
 # Remove image
 docker rmi "$image":latest
 # check removal
-docker image ls |grep "$image"
+docker image ls
 
